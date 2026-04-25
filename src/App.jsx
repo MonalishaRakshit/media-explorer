6128bc7ed78ad700ad7ca87be1390bceba41eacb
Monalisha Rakshit <rakshitmonalisha87@gmail.com>
2026-04-25 23:50:56 +0530

    adding new media search project using redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { fetchGIF, fetchPhotos, fetchVideos } from "./api/MediaApi";
+
+const App = () => {
+  const getPhotoes = async () => {
+    const data = await fetchPhotos("cat");
+    console.log(data.results);
+  };
+
+  return (
+    <div className="h-screen text-white w-full bg-gray-950">
+      <button className="bg-green-700 px-4 py-2 m-5" onClick={getPhotoes}>
+        Get Photos
+      </button>
+
+      <button
+        className="bg-green-700 px-4 py-2 m-5"
+        onClick={async () => {
+          const data = await fetchVideos("cat");
+          console.log(data.videos);
+        }}
+      >
+        Get Videos
+      </button>
+
+      <button
+        className="bg-green-700 px-4 py-2 m-5"
+        onClick={async () => {
+          const data = await fetchGIF("cat");
+          console.log(data);
+        }}
+      >
+        Get GIF
+      </button>
+    </div>
+  );
+};
+
+export default App;
